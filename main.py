ac13b39222ebfc304de6275b972694b964c95e77
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2023-08-08 22:19:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,19 @@
 """
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+This Add-On allows you to bulk delete tags and/or key value pairs from a set of documents. 
 """
 
 from documentcloud.addon import AddOn
 
-
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
+class BulkDeleteTags(AddOn):
+    """DocumentCloud Add-On to bulk remove tags"""
 
     def main(self):
         """The main add-on functionality goes here."""
         # fetch your add-on specific data
-        name = self.data.get("name", "world")
-
-        self.set_message("Hello World start!")
-
-        # add a hello note to the first page of each selected document
+        key = self.data.get("key")
         for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
-
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
-
+            document.data.pop(key)
+            document.save()
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    BulkDeleteTags().main()
